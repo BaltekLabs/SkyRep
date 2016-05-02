60a4930587aca45f7545d49685ecbf2d04f5f769
David Sheaf <David Sheaf>
2016-05-02 12:41:57 -0400

    fixed centering and port add

--- skylane/www/js/app.js
+++ skylane/www/js/app.js
@@ -39,7 +39,7 @@ angular.module('starter', ['ionic', 'starter.controllers','starter.directives'])
     url: '/flight',
     views: {
       'menuContent': {
-        templateUrl: 'templates/flight.html',
+        templateUrl: 'templates/flight.html'
       }
     }
   })
--- skylane/www/js/controllers.js
+++ skylane/www/js/controllers.js
@@ -11,27 +11,68 @@ angular.module('starter.controllers', [])
 })
 
 .controller('MapCtrl', function($scope, $ionicLoading) {
-  $scope.mapCreated = function(map) {
-    $scope.map = map;
-  };
 
-  $scope.centerOnMe = function () {
-    console.log("Centering");
-    if (!$scope.map) {
-      return;
-    }
+    function createMarker(place) {
+      var placeLoc = place.geometry.location;
+      var marker = new google.maps.Marker({
+        map: $scope.map,
+        position: place.geometry.location
+      });
 
-    $scope.loading = $ionicLoading.show({
-      content: 'Getting current location...',
-      showBackdrop: false
-    });
+      google.maps.event.addListener(marker, 'click', function() {
+        var infowindow = new google.maps.InfoWindow();
+        infowindow.setContent(place.name);
+        infowindow.open($scope.map, this);
+      });
+    };
 
-    navigator.geolocation.getCurrentPosition(function (pos) {
-      console.log('Got pos', pos);
-      $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-      $scope.loading.hide();
-    }, function (error) {
-      alert('Unable to get location: ' + error.message);
-    });
-  };
+    function callback(results, status) {
+      console.log('status:' + status);
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        console.log(results);
+        for (var i = 0; i < results.length; i++) {
+          createMarker(results[i]);
+        }
+      }
+    };
+
+    $scope.mapCreated = function(map) {
+      $scope.map = map;
+
+      //Wait until the map is loaded
+      google.maps.event.addListenerOnce($scope.map, 'idle', function(){
+        $scope.centerOnMe();
+      });
+
+    };
+
+    $scope.centerOnMe = function () {
+      console.log("Centering");
+      if (!$scope.map) {
+        return;
+      }
+
+      $scope.loading = $ionicLoading.show({
+        content: 'Getting current location...',
+        showBackdrop: false
+      });
+
+      navigator.geolocation.getCurrentPosition(function (pos) {
+        console.log('Got pos', pos);
+
+        $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+
+        var service = new google.maps.places.PlacesService($scope.map);
+        service.nearbySearch({
+            location:  new google.maps.LatLng(pos.coords.latitude,pos.coords.longitude),
+            radius: '50000',
+            type: ['airport']
+        },callback);
+
+        $scope.loading.hide();
+      }, function (error) {
+        alert('Unable to get location: ' + error.message);
+      });
+    };
+
 });
